2a5aa8a979824038297f7d1365fef1dbe529dc0f
Bill Budge <billbudge@Bills-MacBook-Pro.local>
2023-02-14 12:22:44 -0800

    More tests of DataModel.

--- src/dataModels.ts
+++ src/dataModels.ts
@@ -1,150 +1,117 @@
-// export type ItemVisitor = (item: object) => void;
-// export type AttributeVisitor = (item: object, attr: string | number) => void;
+export type ItemVisitor = (item: object) => void;
 
-// export interface Model {
-//   root: object;
-// }
+export type DataAttr = string | number;
+export type PropertyVisitor = (item: object, attr: DataAttr) => void;
 
-// export class DataModel {
-//   nextId: number = 1;
-//   root: object;
-//   initializers: Array<ItemVisitor> = new Array();
+export interface Model {
+  root: object;
+  dataModel: DataModel;
+}
 
-//   // Returns true iff. value is an item in the model.
-//   isItem(value: any) : boolean {
-//     return value && typeof value === 'object';
-//   }
+export class DataModel {
+  private highestId: number = 0;  // 0 stands for no id.
+  private root_: object;
+  private initializers: Array<ItemVisitor> = new Array();
 
-//   // Returns true iff. item[attr] is a model property.
-//   isProperty(item: object, attr: string | number) : boolean {
-//     return attr !== 'id' &&
-//             item.hasOwnProperty(attr);
-//   }
+  root() : object {
+    return this.root_;
+  }
+  // Returns true iff. item[attr] is a model property.
+  isProperty(item: object, attr: DataAttr) : boolean {
+    return attr !== 'id' &&
+           item.hasOwnProperty(attr);
+  }
 
-//   getId(item: object) : number {
-//     return item['id'];
-//   }
+  getId(item: any) : number {
+    return item.id as number;
+  }
 
-//   assignId(item: object) : number {
-//     // 0 is not a valid id in this model.
-//     const id = this.nextId++;
-//     item['id'] = id;
-//     return id;
-//   }
+  assignId(item: any) : number {
+    // 0 is not a valid id in this model.
+    const id = ++this.highestId;
+    item.id = id;
+    return id;
+  }
 
-//   // Returns true iff. item[attr] is a property that references an item.
-//   isReference(item: object, attr: string | number) {
-//     const attrName = attr.toString(),
-//           position = attrName.length - 2;
-//     return position >= 0 &&
-//             attrName.lastIndexOf('Id', position) === position;
-//   }
+  // Returns true iff. item[attr] is a property that references an item.
+  isReference(item: object, attr: DataAttr) {
+    const attrName = attr.toString(),
+          position = attrName.lastIndexOf('Id');
+    return position === attrName.length - 2;
+  }
 
-//   configure(model: Model) {
-//     const self = this,
-//           root = model.root || model;
-//     this.root = root;
+  // Visits the item's top level properties.
+  visitProperties(item: object, propFn: PropertyVisitor) {
+    if (Array.isArray(item)) {
+      // Array item.
+      const length = item.length;
+      for (let i = 0; i < length; i++)
+        propFn(item, i);
+    } else {
+      // Object.
+      for (let attr in item) {
+        if (this.isProperty(item, attr))
+          propFn(item, attr);
+      }
+    }
+  }
 
-//     // Find the maximum id in the model and set nextId to 1 greater.
-//     this.nextId = 0;
-//     this.visitSubtree(root, function(item: object) {
-//       const id = self.getId(item);
-//       if (id)
-//         this.nextId = Math.max(this.nextId, id);
-//     });
-//     this.nextId++;
-//   }
+  // Visits the item's top level reference properties.
+  visitReferences(item: object, refFn: PropertyVisitor) {
+    const self = this;
+    this.visitProperties(item, function(item, attr) {
+      if (self.isReference(item, attr))
+        refFn(item, attr);
+    });
+  }
 
-//   // Visits the item's top level properties.
-//   visitProperties(item: object, propFn: AttributeVisitor) {
-//     if (Array.isArray(item)) {
-//       // Array item.
-//       const length = item.length;
-//       for (let i = 0; i < length; i++)
-//         propFn(item, i);
-//     } else {
-//       // Object.
-//       for (let attr in item) {
-//         if (this.isProperty(item, attr))
-//           propFn(item, attr);
-//       }
-//     }
-//   }
+  // Visits the item's top level properties that are Arrays or child items.
+  visitChildren(item: object, childFn: ItemVisitor) {
+    const self = this;
+    this.visitProperties(item, function(item: object, attr: DataAttr) {
+      const value = (item as any)[attr];
+      if (Array.isArray(value)) {
+        self.visitChildren(value, childFn);
+      } else if (value instanceof Object) {
+        childFn(value);
+      }
+    });
+  }
 
-//   // Visits the item's top level reference properties.
-//   visitReferences(item: object, refFn: AttributeVisitor) {
-//     const self = this;
-//     this.visitProperties(item, function(item, attr) {
-//       if (self.isReference(item, attr))
-//         refFn(item, attr);
-//     });
-//   }
+  // Visits the item and all of its descendants.
+  visitSubtree(item: object, itemFn: ItemVisitor) {
+    itemFn(item);
+    const self = this;
+    this.visitChildren(item, function(child: object) {
+      self.visitSubtree(child, itemFn);
+    });
+  }
 
-//   // Visits the item's top level properties that are Arrays or child items.
-//   visitChildren(item: object, childFn: ItemVisitor) {
-//     const self = this;
-//     this.visitProperties(item, function(item, attr) {
-//       const value = item[attr];
-//       if (!self.isItem(value))
-//         return;
-//       if (Array.isArray(value))
-//         self.visitChildren(value, childFn);
-//       else
-//         childFn(value);
-//     });
-//   }
+  addInitializer(initialize: ItemVisitor) {
+    this.initializers.push(initialize);
+  }
 
-//   // Visits the item and all of its descendant items.
-//   visitSubtree(item: object, itemFn: ItemVisitor) {
-//     itemFn(item);
-//     const self = this;
-//     this.visitChildren(item, function(child) {
-//       self.visitSubtree(child, itemFn);
-//     });
-//   }
+  initialize(item: object) {
+    const self = this;
+    this.visitSubtree(item, function(subItem) {
+      self.initializers.forEach(function(initializer) {
+        initializer(subItem);
+      });
+    });
+  }
 
-//   addInitializer(initialize: ItemVisitor) {
-//     this.initializers.push(initialize);
-//   }
-
-//   initialize(item: object) {
-//     const self = this,
-//           root = item || this.root;
-//     this.visitSubtree(root, function(item) {
-//       self.initializers.forEach(function(initializer) {
-//         initializer(item);
-//       });
-//     });
-//   }
-
-//   constructor(model: Model) {
-//     this.root = model.root;
-//     model['dataModel'] = this;
-//   }
-
-//   static extend(model: Model) : DataModel {
-//     let instance = model['dataModel'];
-//     if (!instance)
-//       instance = new DataModel(model);
-//     return instance;
-//   }
-
-
-//   // function extend(model, impl) {
-//   //   if (model.dataModel)
-//   //     return model.dataModel;
-
-//   //   const instance = Object.assign(Object.create(proto), impl || defaultImpl);
-//   //   instance.model = model;
-//   //   instance.configure(model);
-
-//   //   instance.initializers = new Array();
-
-//   //   model.dataModel = instance;
-//   //   return instance;
-//   // }
-
-//   // return {
-//   //   extend: extend,
-//   // };
-// }
+  constructor(root: object) {
+    this.root_ = root;
+    const self = this;
+    // Find the highest id in the model.
+    const unidentifed = new Array();
+    this.visitSubtree(root, function(item: object) {
+      const id = self.getId(item);
+      if (!id)
+        unidentifed.push(item);
+      else
+        self.highestId = Math.max(self.highestId, id);
+    });
+    unidentifed.forEach(item => self.assignId(item));
+  }
+}
--- test/dataModels.test.ts
+++ test/dataModels.test.ts
@@ -1,29 +1,147 @@
 // Collections tests.
 
 import {describe, expect, test} from '@jest/globals';
-// import { Model, DataModel } from '../src/dataModels';
+import { Model, DataModel, DataAttr } from '../src/dataModels';
 
 'use strict';
 
 //------------------------------------------------------------------------------
-// LinkedList tests.
+// DataModel tests.
 
-// describe('DataModels', () => {
-//   test('extend', () => {
-//     const model: Model = {
-//             root: {},
-//           },
-//           dataModel = new DataModel(model);
-//     expect(model['dataModel']).toBe(dataModel);
-//     expect(dataModel.root).toBe(model.root);
-//   });
-// });
-/*
+describe('DataModels', () => {
+  test('constructor', () => {
+    const root: any = {
+            child: {
+              id: 1000,
+            },
+            children: new Array({}, {}),  // two unidentified children
+          },
+          dataModel = new DataModel(root),
+          model: Model = {
+            root: root,
+            dataModel: dataModel,
+          };
+    expect(dataModel.root()).toBe(model.root);
+    expect(dataModel.getId(root.child)).toBe(1000);  // preserved
+    expect(dataModel.getId(root)).toBe(1001);
+    expect(dataModel.getId(root.children[0])).toBe(1002);
+    expect(dataModel.getId(root.children[1])).toBe(1003);
+  });
+  test('assignId', () => {
+    const root: any = {
+            id: 1000,
+            child: {
+              id: 1020,
+            },
+          },
+          dataModel = new DataModel(root),
+          model: Model = {
+            root: root,
+            dataModel: dataModel,
+          },
+          child2: any = {};
+    expect(dataModel.assignId(child2)).toBe(1021);
+  });
+  test('isProperty', () => {
+    const root: any = {
+            id: 1,
+            foo: 'foo',
+            child: {
+              id: 2,
+            }
+          },
+          dataModel = new DataModel(root),
+          model: Model = {
+            root: root,
+            dataModel: dataModel,
+          };
 
-QUnit.test("dataModel extend", function() {
-  const model = { root: {} };
-  const test = dataModels.dataModel.extend(model);
-  QUnit.assert.deepEqual(test, model.dataModel);
+    expect(dataModel.isProperty(root, 'foo')).toBe(true);
+    expect(dataModel.isProperty(root, 'bar')).toBe(false);
+    expect(dataModel.isProperty(root, 'id')).toBe(false);
+    expect(dataModel.isProperty(root, 'child')).toBe(true);
+  });
+  test('isReference', () => {
+    const root: any = {
+            id: 1,
+            childId: 2,
+            foo: 'foo',
+            child: {
+              id: 2,
+            }
+          },
+          dataModel = new DataModel(root),
+          model: Model = {
+            root: root,
+            dataModel: dataModel,
+          };
+
+    expect(dataModel.isReference(root, 'foo')).toBe(false);
+    expect(dataModel.isReference(root, 'bar')).toBe(false);
+    expect(dataModel.isReference(root, 'childId')).toBe(true);
+    expect(dataModel.isReference(root.child, 'id')).toBe(false);
+  });
+  test('visitProperties', () => {
+    const root: any = {
+            id: 1,
+            childId: 2,
+            foo: 'foo',
+            child: {
+              id: 2,
+              bar: 'baz',
+            },
+            props: new Array(0, 1, 2),
+          },
+          dataModel = new DataModel(root),
+          model: Model = {
+            root: root,
+            dataModel: dataModel,
+          };
+
+    const rootProps = new Array<DataAttr>();
+    dataModel.visitProperties(root, (item, attr) => { rootProps.push(attr)});
+    expect(rootProps.toString()).toBe('childId,foo,child,props');
+  });
+  test('visitReferences', () => {
+    const root: any = {
+            id: 1,
+            childId: 2,
+            fooId: 3,
+            child: {
+              id: 2,
+              bar: 'baz',
+            },
+          },
+          dataModel = new DataModel(root),
+          model: Model = {
+            root: root,
+            dataModel: dataModel,
+          };
+
+    const rootRefs = new Array<DataAttr>();
+    dataModel.visitReferences(root, (item, attr) => { rootRefs.push(attr)});
+    expect(rootRefs.toString()).toBe('childId,fooId');
+  });
+  test('visitChildren, visitSubtree', () => {
+    const root: any = {
+            child: {
+              id: 1000,
+            },
+            children: new Array({ grandchild: {} }, {}),  // two unidentified children
+          },
+          dataModel = new DataModel(root),
+          model: Model = {
+            root: root,
+            dataModel: dataModel,
+          };
+    const children = new Array<object>();
+    dataModel.visitChildren(root, (item) => { children.push(item)});
+    expect(children).toEqual(
+        new Array<object>(root.child, root.children[0], root.children[1]));
+    const descendants = new Array<object>();
+    dataModel.visitSubtree(root, (item) => { descendants.push(item)});
+    expect(descendants).toEqual(
+        new Array<object>(root, root.child, root.children[0], root.children[0].grandchild, root.children[1]));
+  });
+
 });
-
-*/
