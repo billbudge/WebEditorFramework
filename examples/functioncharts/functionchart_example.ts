574f8ab390533631952d2bfc5632c8d7918e13b6
Bill Budge <billbudge@Bills-MacBook-Pro.local>
2023-06-13 17:50:46 -0700

    Add some files to version control.

---
+++ examples/functioncharts/functionchart_example.ts
@@ -0,0 +1,50 @@
+import { getDefaultTheme, CanvasController, PropertyGridController } from '../../src/diagrams.js'
+import { FunctionchartEditor } from '../../examples/functioncharts/functioncharts.js'
+
+(function() {
+
+  const body = document.getElementById('body'),
+        canvas = document.getElementById('canvas'),
+        palette = document.getElementById('palette');
+  if (body && canvas && palette) {
+
+        // TODO proper "document" handling.
+    const theme = getDefaultTheme(),  // or getBlueprintTheme
+          canvasController = new CanvasController(canvas as HTMLCanvasElement),
+          paletteController = new CanvasController(palette as HTMLCanvasElement),
+          propertyGridController = new PropertyGridController(body, theme),
+          functionchartEditor = new FunctionchartEditor(
+              theme, canvasController, paletteController, propertyGridController);
+
+    document.bgColor = theme.bgColor;
+    palette.style.backgroundColor = theme.altBgColor;
+    palette.style.borderColor = theme.strokeColor;
+    palette.style.borderStyle = 'solid';
+    palette.style.borderWidth = '0.25px';
+    canvas.style.backgroundColor = theme.bgColor;
+
+    canvasController.configure([functionchartEditor]);
+    canvasController.setSize(window.innerWidth, window.innerHeight);
+    paletteController.configure([functionchartEditor]);
+    paletteController.setSize(150, 128);
+    paletteController.draggable = true;
+
+    window.onbeforeunload = function() {
+      return "Are you sure you want to navigate away?";
+    }
+
+    window.onresize = function() {
+      paletteController.onWindowResize();
+      canvasController.onWindowResize();
+    }
+
+    document.addEventListener('keydown', function(e) {
+      // Handle any keyboard commands for the app window here.
+      canvasController.onKeyDown(e);
+    });
+
+    document.addEventListener('keyup', function(e) {
+      canvasController.onKeyUp(e);
+    });
+  }
+})();
